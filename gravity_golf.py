ed6890b35b7b6640ee79d4a15e84af5069e967f1
kailevy <kai0levy@gmail.com>
2015-03-26 15:05:34 -0400

    Setting up class framework

--- gravity_golf.py
+++ gravity_golf.py
@@ -8,7 +8,43 @@ import pygame
 import os
 import numpy as np
 
+# Screen dimensions
+SCREEN_WIDTH  = 800
+SCREEN_HEIGHT = 600
 
+# Colors
+BLACK    = (   0,   0,   0)
+WHITE    = ( 255, 255, 255)
+BLUE     = (   0,   0, 255)
+RED      = ( 255,   0,   0)
+GREEN    = (   0, 255,   0)
+
+CURR_DIR = os.path.dirname(os.path.realpath(__file__))
+
+class GolfModel():
+    """Represents the game state"""
+    def __init__(self):
+        pass 
+
+class GolfView():
+    """Represents the view of the game"""
+    def __init__(self,model):
+        pass 
+
+class GolfGame():
+    """Runs the game"""
+    def __init__(self):
+        pass 
+
+class Ball(pygame.sprite.Sprite):
+    """Represents a ball, for extension into: game ball, gravity ball..."""
+    def __init__(self):
+        pass 
+
+class Tile(pygame.sprite.Sprite):
+    """Represents a tile, for extension into: friction tile, wall tile, acceleration tile..."""
+    def __init__(self):
+        pass 
 
 if __name__ == '__main__':
-    
+    pass
