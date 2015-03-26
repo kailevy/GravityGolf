c1addc0f87815c99a621eeed29c1ec408df3584b
kailevy <kai0levy@gmail.com>
2015-03-26 14:48:35 -0400

    File initialization

---
+++ gravity_golf.py
@@ -0,0 +1,14 @@
+"""
+Patrick Huston, Kai Levy
+Mechanics with Zhenya, Spring 2015
+Physics-Enhanced Mini Golf
+"""
+
+import pygame
+import os
+import numpy as np
+
+
+
+if __name__ == '__main__':
+    
