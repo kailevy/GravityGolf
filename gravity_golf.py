9e2567985c1665fdada4b9a0952a1c31df903f3f
Patrick Huston <patrickhuston1@gmail.com>
2015-03-30 11:20:04 -0400

    Updates model and view components

--- gravity_golf.py
+++ gravity_golf.py
@@ -20,26 +20,85 @@ RED      = ( 255,   0,   0)
 GREEN    = (   0, 255,   0)
 
 CURR_DIR = os.path.dirname(os.path.realpath(__file__))
-
+particle moving in the xy
 class GolfModel():
     """Represents the game state"""
-    def __init__(self):
-        pass 
+    def __init__(self, width, height):
+        self.levels = all_levels
+        self.current_level = 0
+        self.width = width
+        self.height = height
+        self.tiles = pygame.sprite.Group()
+        self.level = Level(self.levels[current_level])
+
+    def update(self, delta_t):
+    	self.ball.update(delta_t)
 
 class GolfView():
     """Represents the view of the game"""
-    def __init__(self,model):
+    def __init__(self, model):
         pass 
+
+class GolfController():
+	""" Represents controller for Gravity Golf Game """
+	def __init__(self, model):
+		self.model = model
+
+	def process_events(self):
+		done = False
+		pygame.event.pump()
+
+
+		for event in pygame.event.get():
+
+			#Define events that game elements should respond to
+
+		return done
 
 class GolfGame():
     """Runs the game"""
     def __init__(self):
-        pass 
+        self.model = GolfModel(SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.view = GolfView(self.model, SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.controller = GolfController(self.model)
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+    	""" Run the game """
+    	last_ticks = 0.0
+    	done = False
+
+    	while not done:
+    		t = pygame.time.get_ticks() # Get current time
+    		dt = (t - last_ticks) / 1000.0
+    		last_ticks = t
+
+    		done = self.controller.process_events() # Process all events
+    		
 
 class Ball(pygame.sprite.Sprite):
     """Represents a ball, for extension into: game ball, gravity ball..."""
     def __init__(self):
         pass 
+
+class Level(pygame.sprite.Sprite):
+	""" Represents a level """
+	def __init__(self, map):
+		pygame.sprite.Sprite.__init__(self)
+		self.map = map
+		self.tiles = pygame.sprite.Group()
+
+		#Creates map of tiles from inputted np array
+		for row in xrange(len(self.map)):
+			for x in xrange(len(self.map[row])):
+
+				#For each element in array, create relevant tile
+
+				#TODO: define all tile class(es)
+
+	def draw(self, screen):
+		for tile in self.tiles:
+			tile.draw(screen)
 
 class Tile(pygame.sprite.Sprite):
     """Represents a tile, for extension into: friction tile, wall tile, acceleration tile..."""
