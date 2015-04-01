aa2207ad41edcb4e56dff8d65e5888d9c0c42cfb
Patrick Huston <patrickhuston1@gmail.com>
2015-04-01 15:46:51 -0400

    Completes first basic iteration

--- gravity_golf.py
+++ gravity_golf.py
@@ -36,6 +36,8 @@ level0 = np.array(
     (1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
     (1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)])
 
+all_levels = [level0]
+
 class GolfModel():
     """Represents the game state"""
     def __init__(self, width, height):
@@ -43,8 +45,8 @@ class GolfModel():
         self.current_level = 0
         self.width = width
         self.height = height
-        self.tiles = pygame.sprite.Group()
-        self.level = Level(self.levels[current_level])
+        self.level = Level(self.levels[self.current_level])
+        self.ball = Ball(100, 100, self.level.tiles)
 
     def update(self, delta_t):
     	self.ball.update(delta_t)
@@ -64,20 +66,17 @@ class GolfView():
         pygame.display.update()
 
 class GolfController():
-	""" Represents controller for Gravity Golf Game """
-	def __init__(self, model):
-		self.model = model
+    """ Represents controller for Gravity Golf Game """
+    def __init__(self, model):
+        self.model = model
 
-	def process_events(self):
-		done = False
-		pygame.event.pump()
+    def process_events(self):
+        done = False
+        pygame.event.pump
 
-
-		# for event in pygame.event.get():
-
-			#Define events that game elements should respond to
-
-		return done
+        # for event in pygame.event.get():
+        #     continue
+        return done
 
 class GolfGame():
     """Runs the game"""
@@ -92,16 +91,16 @@ class GolfGame():
     	last_ticks = 0.0
     	done = False
 
-    	while not done:
-    		t = pygame.time.get_ticks() # Get current time
-    		dt = (t - last_ticks) / 1000.0
-    		last_ticks = t
+        while not done:
+            t = pygame.time.get_ticks() # Get current time
+            dt = (t - last_ticks) / 1000.0
+            last_ticks = t
 
-    		done = self.controller.process_events() # Process all events
-    		self.model.update(dt) # Update model based on events
-    		self.view.draw() # Draw view
+            done = self.controller.process_events() # Process all events
+            self.model.update(dt) # Update model based on events
+            self.view.draw() # Draw view
 
-    		self.clock.tick(25) # Change in time
+            self.clock.tick(25) # Change in time
 
 
 class Ball(pygame.sprite.Sprite):
@@ -115,12 +114,15 @@ class Ball(pygame.sprite.Sprite):
 
         self.pos_x = pos_x
         self.pos_y = pos_y
-        self.vel_x = 0
-        self.vel_y = 0
+        self.vel_x = 100
+        self.vel_y = 100
         self.acc_x = 0
         self.acc_y = 0
 
         self.tiles = tiles
+
+        print "tiles:" 
+        print self.tiles
         self.onGround = False
 
         self.image = pygame.image.load('img/ball.png')
@@ -143,29 +145,55 @@ class Ball(pygame.sprite.Sprite):
         self.rect.y += self.vel_y*dt
         self.collide(0, self.vel_y)
 
+    def collide(self, vel_x, vel_y):
+        """ Handle collisions between ball and walls """
+
+        for tile in self.tiles:
+
+            if pygame.sprite.collide_rect(self, tile):
+                print "collision!"
+                if isinstance(tile, ExitTile):
+                    pass
+                    #TODO: implement going to next level event
+
+                if vel_x > 0 and isinstance(tile, WallTile):
+                    self.rect.right = tile.rect.left
+                    self.vel_x *= -1
+
+                if vel_x < 0 and isinstance(tile, WallTile):
+                    self.rect.left = tile.rect.right
+                    self.vel_x *= -1
+
+                if vel_y > 0 and isinstance(tile, WallTile):
+                    self.rect.bottom = tile.rect.top
+                    self.vel_y *= -1
+
+                if vel_y < 0 and isinstance(tile, WallTile):
+                    self.rect.top = tile.rect.bottom
+                    self.vel_y *= -1
+
 class Level(pygame.sprite.Sprite):
-	""" Represents a level """
-	def __init__(self, map):
-		pygame.sprite.Sprite.__init__(self)
-		self.map = map
-		self.tiles = pygame.sprite.Group()
+    """ Represents a level """
+    def __init__(self, map):
+        pygame.sprite.Sprite.__init__(self)
+        self.map = map
+        self.tiles = pygame.sprite.Group()
 
 		#Creates map of tiles from inputted np array
-		for row in xrange(len(self.map)):
-			for x in xrange(len(self.map[row])):
-				#For each element in array, create relevant tile
+        for row in xrange(len(self.map)):
+            for x in xrange(len(self.map[row])):
+                #For each element in array, create relevant tile
 
-				if self.map[row][x] == 1:
-                    tile = Tile(x*50, row*50)
-                    self.tiles.add(tile)
                 if self.map[row][x] == 1:
-                    tile = Tile(x*50, row*50)
+                    tile = WallTile(x*50, row*50)
+                    self.tiles.add(tile)
+                if self.map[row][x] == 2:
+                    tile = ExitTile(x*50, row*50)
                     self.tiles.add(tile)
 
-
-	def draw(self, screen):
-		for tile in self.tiles:
-			tile.draw(screen)
+    def draw(self, screen):
+        for tile in self.tiles:
+            tile.draw(screen)
 
 class Tile(pygame.sprite.Sprite):
     """Represents a tile, for extension into: friction tile, wall tile, acceleration tile..."""
@@ -175,25 +203,30 @@ class Tile(pygame.sprite.Sprite):
     	self.x_pos = x_pos
     	self.y_pos = y_pos
 
-    	self.image = pygame.image.load('img/wallTile.png')
-    	self.rect = self.image.get_rect()
+    def draw(self, screen):
+        screen.blit(self.image.convert_alpha(), self.rect)
 
-    	self.rect = self.rect.move(self.x_pos, self.y_pos)
+class WallTile(Tile):
+    """ Represents a wall tile that the golf ball will collide with """
+    def __init__(self, x_pos, y_pos):
+        Tile.__init__(self, x_pos, y_pos)
 
-	def draw(self, screen):
-		screen.blit(self.image.convert_alpha(), self.rect)
+        self.image = pygame.image.load('img/wallTile.png')
+        self.rect = self.image.get_rect()
+
+        self.rect = self.rect.move(self.x_pos, self.y_pos)
 
 class AccelTile(Tile):
-	def __init__(self, x_pos, y_pos, acceleration):
-		Tile.__init__(self, x_pos, y_pos)
-		if acceleration == 0:
-			self.image = pygame.image.load('img/negAccelTile')
-		if acceleration == 1:
-			self.image = pygame.image.load('img/posAccelTile')
-		if acceleration == 2:
-			self.image = pygame.image.load('img/rightAccelTile')
-		if acceleration == 3:
-			self.image = pygame.image.load('img/leftAccelTile') 
+    def __init__(self, x_pos, y_pos, acceleration):
+        Tile.__init__(self, x_pos, y_pos)
+        if acceleration == 0:
+            self.image = pygame.image.load('img/negAccelTile')
+        if acceleration == 1:
+            self.image = pygame.image.load('img/posAccelTile')
+        if acceleration == 2:
+            self.image = pygame.image.load('img/rightAccelTile')
+        if acceleration == 3:
+            self.image = pygame.image.load('img/leftAccelTile') 
 
         self.rect = self.image.get_rect()
         self.rect = self.rect.move(self.x_pos, self.y_pos)
